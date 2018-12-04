0f00ecac192085e7eaff9800a2dde85ac96e03c5
Smoki-PC\Smoki <matko.smoljan@gmail.com>
2018-12-04 22:16:33 +0100

    (90) Fixing up lagg issues by simulating moves on a client

--- KrazyKarts/Source/KrazyKarts/GoKart.cpp
+++ KrazyKarts/Source/KrazyKarts/GoKart.cpp
@@ -6,12 +6,14 @@
 
 #include "Engine/World.h"
 #include "DrawDebugHelpers.h"
+#include "UnrealNetwork.h"
 
 // Sets default values
 AGoKart::AGoKart()
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
 }
 
@@ -20,7 +22,18 @@ void AGoKart::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (HasAuthority())
+	{
+		NetUpdateFrequency = 1;
+	}
 }
+
+void AGoKart::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const		// ne mijenjaj ime argumenta
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);		// potrebno je pozvati 'Super' metodu kako bi ispravno radili ispisi actor roles
+	DOREPLIFETIME(AGoKart, ServerState);						// ovaj makro prihvaca varijablu za replikaciju. Replicira 'ServerState'; kad god se ta varijabla postavi na serveru, svi klijenti ce vidjeti tu vrijednost
+																// i moci postaviti svoju vrijednost transformacije na tu vrijednost
+}														
 
 FString GetEnumText(ENetRole Role)
 {
@@ -44,20 +57,71 @@ void AGoKart::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 	
-	FVector Force = GetActorForwardVector() * MaxDrivingForce * Throttle;
+	if (IsLocallyControlled())
+	{
+		FGoKartMove Move = CreateMove(DeltaTime);
+		if (!HasAuthority())					// nema potrebe dodavati pokrete u queue ako se radi o serveru
+		{
+			UnacknowledgedMoves.Add(Move);
+			UE_LOG(LogTemp, Warning, TEXT("Queue Length: %d"), UnacknowledgedMoves.Num());
+		}
+
+		Server_SendMove(Move);				// saljemo serveru nas konstruirani 'Move'
+
+		SimulateMove(Move);					// moramo pozvati i lokalno inace se ne simuliraju pokreti nego kart mijenja stanja prema stanjima dobivenih iz OnRep_ServerState()	
+	}
+
+	DrawDebugString(GetWorld(), FVector(0, 0, 100), GetEnumText(Role), this, FColor::White, DeltaTime);
+}
+
+void AGoKart::OnRep_ServerState()				// ovo ce se pozvati samo na klijentu kada je doslo do promjene stoga ne treba provjeravati 'HasAuthority()'
+{
+	SetActorTransform(ServerState.Transform);				// postavi transformaciju klijenta prema onoj koja se nalazi na serveru
+	Velocity = ServerState.Velocity;						// Resetiramo 'Velocity' na klijentu na sto god server kaze da je tocno
+	ClearAcknowledgedMoves(ServerState.LastMove);
+}
+
+void AGoKart::SimulateMove(FGoKartMove Move)
+{
+	FVector Force = GetActorForwardVector() * MaxDrivingForce * Move.Throttle;
 
 	Force += GetAirResistance();
 	Force += GetRollingResistance();
 
 	FVector Acceleration = Force / Mass;
 
-	Velocity = Velocity + Acceleration * DeltaTime;
+	Velocity = Velocity + Acceleration * Move.DeltaTime;
 
-	ApplyRotation(DeltaTime);
+	ApplyRotation(Move.DeltaTime, Move.SteeringThrow);
 
-	UpdateLocationFromVelocity(DeltaTime);
+	UpdateLocationFromVelocity(Move.DeltaTime);				// ideja je da se svaki frame: na klijentu simulira stvarna transformacija i na serveru ista postavlja. Kada dode do promjene na serveru (sto se dogada pozivom 
+															// OnRep_ReplicatedTransform()) mijenja se transformacija na klijentu prema onoj koja je zabiljezena na serveru.
+}
 
-	DrawDebugString(GetWorld(), FVector(0, 0, 100), GetEnumText(Role), this, FColor::White, DeltaTime);
+FGoKartMove AGoKart::CreateMove(float DeltaTime)
+{
+	FGoKartMove Move;
+	Move.DeltaTime = DeltaTime;
+	Move.SteeringThrow = SteeringThrow;
+	Move.Throttle = Throttle;
+	Move.Time = GetWorld()->TimeSeconds;
+
+	return Move;
+}
+
+void AGoKart::ClearAcknowledgedMoves(FGoKartMove LastMove)
+{
+	TArray<FGoKartMove> NewMoves;
+
+	for (const FGoKartMove& Move : UnacknowledgedMoves)
+	{
+		if (Move.Time > LastMove.Time)
+		{
+			NewMoves.Add(Move);
+		}
+	}
+
+	UnacknowledgedMoves = NewMoves;
 }
 
 FVector AGoKart::GetAirResistance()
@@ -72,7 +136,7 @@ FVector AGoKart::GetRollingResistance()
 	return -Velocity.GetSafeNormal() * RollingResistanceCoef * NormalForce;
 }
 
-void AGoKart::ApplyRotation(float DeltaTime)
+void AGoKart::ApplyRotation(float DeltaTime, float SteeringThrow)
 {
 	float DeltaLocation = FVector::DotProduct(GetActorForwardVector(), Velocity) * DeltaTime;		// ako ukomponiramo brzinu u jednadzbu onda onemogucimo okretanje u mjestu. DotProduct nam vraca float koji oznacuje
 																									// koliko 'velocity' se nalazi u forward vektoru. Ako je velocity u suprotnom smjeru, onda je taj float negativan
@@ -87,7 +151,7 @@ void AGoKart::ApplyRotation(float DeltaTime)
 
 void AGoKart::UpdateLocationFromVelocity(float DeltaTime)
 {
-	FVector Translation = Velocity * DeltaTime * 100;		// mnozimo da dobijemo metre u sekundi
+	FVector Translation = Velocity * DeltaTime * 100;		// mnozimo sa 100 da dobijemo metre u sekundi
 
 	FHitResult OutSweepHitResult;
 	AddActorWorldOffset(Translation, true, &OutSweepHitResult);						// postavljamo mu novu lokaciju i postavljamo 'sweep'. Sweep provjerava koliziju prilikom pomicanja
@@ -111,31 +175,25 @@ void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 void AGoKart::MoveForward(float Value)
 {
 	Throttle = Value;
-	Server_MoveForward(Value);									// zatim updateamo poziciju na serveru
 }
 
 void AGoKart::MoveRight(float Value)
 {
 	SteeringThrow = Value;
-	Server_MoveRight(Value);
+	
 }
 
-void AGoKart::Server_MoveForward_Implementation(float Value)	// Na serveru se zapravo izvrsava ova funkcija post-fixana sa _Implementation(ne Server_MoveForward).
-{
-	Throttle = Value;
+void AGoKart::Server_SendMove_Implementation(FGoKartMove Move)		// Na serveru se zapravo izvrsava ova funkcija post-fixana sa _Implementation(ne Server_MoveForward). Salju se komponente 'Move' strukture koje ce
+{																	// na serveru simulirati kretanje
+	SimulateMove(Move);
+
+	ServerState.LastMove = Move;
+	ServerState.Transform = GetActorTransform();					// postavljamo transformaciju actora na serveru svaki frame i omogucavamo ju za replikaciju
+	ServerState.Velocity = Velocity;						
 }
 
-bool AGoKart::Server_MoveForward_Validate(float Value)			// potrebno za anti-cheat
+bool AGoKart::Server_SendMove_Validate(FGoKartMove Move)			// potrebno za anti-cheat
 {
-	return FMath::Abs(Value) <= 1;								// vrati true samo ako je vrijednost gasa izmedu 0 i 1. Sve ostalo je ne moguce i smatra se varanjem
+	return true;								// TODO; make better validation
 }
 
-void AGoKart::Server_MoveRight_Implementation(float Value)
-{
-	SteeringThrow = Value;
-}
-
-bool AGoKart::Server_MoveRight_Validate(float Value)		
-{
-	return FMath::Abs(Value) <= 1;
-}
--- KrazyKarts/Source/KrazyKarts/GoKart.h
+++ KrazyKarts/Source/KrazyKarts/GoKart.h
@@ -6,6 +6,35 @@
 #include "GameFramework/Pawn.h"
 #include "GoKart.generated.h"
 
+USTRUCT()
+struct FGoKartMove
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY()
+	float Throttle;
+	UPROPERTY()
+	float SteeringThrow;
+	UPROPERTY()
+	float DeltaTime;
+	UPROPERTY()
+	float Time;			// vrijeme kada se 'Move' dogodio
+};
+
+
+USTRUCT()
+struct FGoKartState					// bolje je sve komponente 'stanja' actora spremiti u strukturu koja se replicira. Na taj nacini prilikom promjene jedne varijable, cijela se struktura updatea
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY()
+	FGoKartMove LastMove;
+	UPROPERTY()
+	FVector Velocity;
+	UPROPERTY()			
+	FTransform Transform;
+};
+
 UCLASS()
 class KRAZYKARTS_API AGoKart : public APawn
 {
@@ -27,6 +56,20 @@ public:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 private:
+	void SimulateMove(FGoKartMove Move);
+
+	FGoKartMove CreateMove(float DeltaTime);
+	void ClearAcknowledgedMoves(FGoKartMove LastMove);
+
+	FVector GetAirResistance();
+	FVector GetRollingResistance();
+
+	void ApplyRotation(float DeltaTime, float SteeringThrow);
+	void UpdateLocationFromVelocity(float DeltaTime);
+
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+
 	// Masa auta (kg).
 	UPROPERTY(EditAnywhere)
 	float Mass = 1000;
@@ -47,22 +90,19 @@ private:
 	UPROPERTY(EditAnywhere)
 	float RollingResistanceCoef = 0.015;		// vrijednost preuzeta iz dokumentacije
 
+	UFUNCTION(Server, Reliable, WithValidation)			// RPC funkcija (Remote Procedure Call) - poziva se na klijentu, a izvršava se na serveru. Svi RPCs su unreliable po pitanju izvrsavanja, 				
+	void Server_SendMove(FGoKartMove Move);				// stoga ako zelimo da se funckija izvrsi dodamo 'Reliable'. Pre-fixed sa Server_ jer se izvrsava na serveru.
+
+	UPROPERTY(ReplicatedUsing = OnRep_ServerState)		// 'ReplicatedUsing' omogucava da se struktura moze replicirati sa servera i da se sazna kada je doslo do promjene te varijable na serveru te se pri
+	FGoKartState ServerState;							// promjeni strukture poziva zadana funkcija (u ovom slucaju: OnRep_ServerState). U toj funkciji cemo updateati transformaciju client actora 
+
+	FVector Velocity;
+							
+	UFUNCTION()														// mora biti UFUNCTION()
+	void OnRep_ServerState();
+
 	float Throttle;
 	float SteeringThrow;
-	FVector Velocity;
-	
-	FVector GetAirResistance();
-	FVector GetRollingResistance();
 
-	void ApplyRotation(float DeltaTime);
-	void UpdateLocationFromVelocity(float DeltaTime);
-
-	void MoveForward(float Value);
-	void MoveRight(float Value);
-
-	UFUNCTION(Server, Reliable, WithValidation)			// RPC funkcija (Remote Procedure Call) - poziva se na klijentu, a izvršava se na SERVER-u. Svi RPCs su unreliable po pitanju izvrsavanja, 				
-	void Server_MoveForward(float Value);				// stoga ako zelimo da se funckija izvrsi dodamo 'Reliable'. Pre-fixed sa Server_ jer se izvrsava na serveru.
-
-	UFUNCTION(Server, Reliable, WithValidation)
-	void Server_MoveRight(float Value);
+	TArray<FGoKartMove> UnacknowledgedMoves;
 };
